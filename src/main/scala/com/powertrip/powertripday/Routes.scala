fbda5c1030b3b09d613ccda044ff988c7270c72e
Felix Brousseau <felix.brousseau-tremblay.1@live.com>
2019-11-20 14:55:33 +0100

    setup helm chart

--- src/main/scala/com/powertrip/powertripday/Routes.scala
+++ src/main/scala/com/powertrip/powertripday/Routes.scala
@@ -13,7 +13,7 @@ import org.http4s.implicits._
 object Routes {
   def dayRoutes(): Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes.of[IO] {
     case GET -> Root / "days" / _ =>
-      val day = Day(date=LocalDateTime.now(), activities=NonEmptyList.of(1))
+      val day = Day(date=LocalDateTime.now(), activities=NonEmptyList.of(1, 2))
       for {
         response <- Ok(day)
       } yield response
