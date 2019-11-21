9b182dbce0f628557c22ac5b1197163d8a2c5dec
Felix Brousseau <felix.brousseau-tremblay.1@live.com>
2019-11-21 21:58:07 +0100

    add versioning

--- src/main/scala/com/powertrip/day/Main.scala
+++ src/main/scala/com/powertrip/day/Main.scala
@@ -2,18 +2,22 @@ package com.powertrip.day
 
 import cats.effect._
 import cats.implicits._
-import fs2.Stream
+import fs2.{INothing, Stream}
+import org.http4s.implicits._
 import org.http4s.server.blaze._
 import org.http4s.server.middleware.Logger
 import cats.Applicative
+import org.http4s.server.Router
 
 object Main extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     stream[IO].compile.drain.as(ExitCode.Success)
 
-  def stream[F[_]: ConcurrentEffect: Applicative: ContextShift: Timer] = {
-    val httpApp = Routes.dayRoutes[F]()
-    val enhanced = Logger.httpApp(true, true)(httpApp)
+  def stream[F[_]: ConcurrentEffect: Applicative: ContextShift: Timer]: Stream[F, INothing] = {
+    val httpApp = Router(
+      "/v1" -> new Routes[F].routes
+    ).orNotFound
+    val enhanced = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
     for {
       exitCode <- BlazeServerBuilder[F]
         .bindHttp(8081, "0.0.0.0")
--- src/main/scala/com/powertrip/day/Routes.scala
+++ src/main/scala/com/powertrip/day/Routes.scala
@@ -2,28 +2,17 @@ package com.powertrip.day
 
 import java.time.LocalDateTime
 
-import cats.data.{Kleisli, NonEmptyList}
-import cats.effect.IO
-import cats.effect.Sync
+import cats.data.NonEmptyList
+import cats.effect._
 import com.powertrip.day.Day._
-import io.circe.syntax._
 import org.http4s._
-import org.http4s.dsl.io._
-import org.http4s.implicits._
 import org.http4s.dsl.Http4sDsl
 
-object Routes {
-  def dayRoutes[F[_]: Sync]() = {
-    val dsl = new Http4sDsl[F] {}
-    import dsl._
 
-    HttpRoutes
-      .of[F] {
-        case GET -> Root / "days" / _ =>
-          val day = Day(LocalDateTime.now(), NonEmptyList.of(1, 2))
-          Ok(day)
-      }
-      .orNotFound
+class Routes[F[_]: Sync] extends Http4sDsl[F] {
+  val routes: HttpRoutes[F] = HttpRoutes.of[F] {
+    case GET -> Root / "days" / _ =>
+      val day = Day(LocalDateTime.now(), NonEmptyList.of(1, 2, 3))
+      Ok(day)
   }
-
 }
