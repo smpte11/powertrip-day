0bb11a349c19180cb25f6b052b467da62d81269a
Felix Brousseau <felix.brousseau-tremblay.1@live.com>
2019-12-01 17:46:19 +0100

    Add unit test

--- src/main/scala/com/powertrip/day/package.scala
+++ src/main/scala/com/powertrip/day/package.scala
@@ -1,12 +1,24 @@
 package com.powertrip
 
 import cats.Applicative
+import eu.timepit.refined._
+import eu.timepit.refined.api._
+import eu.timepit.refined.numeric._
 import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
 import org.http4s.EntityEncoder
 import org.http4s.circe.jsonEncoderOf
 
 package object day {
+  // Refined primitive types
+  type Lat = Float Refined Interval.Closed[W.`-90.0f`.T, W.`90.0f`.T]
+  type Long = Float Refined Interval.Closed[W.`-180.0f`.T, W.`180.0f`.T]
+
+  // Doamin Value Classes
+  case class Coordinate(lat: Lat, long: Long)
+  case class Location(id: Int Refined Positive, coordinates: Coordinate)
+
+  // Encoders/Decoders Implicits
   implicit val dayEncoder: Encoder[Day] = deriveEncoder[Day]
   implicit def dayEntityEncoder[F[_]: Applicative]: EntityEncoder[F, Day] =
     jsonEncoderOf[F, Day]
--- src/test/scala/com/powertrip/day/DaySpec.scala
+++ src/test/scala/com/powertrip/day/DaySpec.scala
@@ -1,3 +1,13 @@
 package com.powertrip.day
 
-class DaySpec { }
+import org.scalatest.wordspec.AnyWordSpec
+
+class DaySpec extends AnyWordSpec {
+  "Day service" when {
+    "using GET one" should {
+      "return the specified day" in {
+        ???
+      }
+    }
+  }
+}
